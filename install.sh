a30b6d866ed58dad6b9fddde628df323ddc66b5a
Injae Kang <abcinje@gmail.com>
2020-04-23 07:24:13 +0000

    Create gitconfig

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo cp $CURR_DIRPATH/colors/* /usr/share/vim/vim*/colors/
 
 HOME_DIRPATH=$(realpath --relative-to=$HOME $CURR_DIRPATH)
 ln -fs $HOME_DIRPATH/vimrc $HOME/.vimrc
+ln -fs $HOME_DIRPATH/gitconfig $HOME/.gitconfig
