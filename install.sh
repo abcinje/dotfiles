1a0ce12f17d58d74abf28ad09d5ef9bb643567d3
Injae Kang <abcinje@gmail.com>
2020-08-06 16:36:10 +0000

    Remove colors

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 CURR_DIRPATH=$(dirname $0)
-sudo cp $CURR_DIRPATH/colors/* /usr/share/vim/vim*/colors/
-
 HOME_DIRPATH=$(realpath --relative-to=$HOME $CURR_DIRPATH)
 ln -fs $HOME_DIRPATH/vimrc $HOME/.vimrc
 ln -fs $HOME_DIRPATH/gitconfig $HOME/.gitconfig
