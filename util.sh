cf078c2f20aa8471615bbfd512b01d05dbf33010
Injae Kang <abcinje@gmail.com>
2021-03-11 04:11:46 +0900

    Update util.sh

--- util.sh
+++ util.sh
@@ -1,11 +1,9 @@
 function link_file {
   local src=$1 dst=$2
 
-  if [ -z "$(diff -q $src $dst)" ]; then
-    return
-  fi
-
-  if [ -e $dst ]; then
+  if [ ! -e $dst ]; then
+    ln -fs $src $dst
+  elif [ -n "$(diff -q $src $dst)" ]; then
     echo "File \"$dst\" already exists!"
     echo -n "Do you want to overwrite? [y/n] "
     read action
