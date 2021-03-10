d30d98791ec68cd8cfacffe10acd5dc1b509f707
Injae Kang <abcinje@gmail.com>
2021-03-11 02:10:09 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,18 @@
 function link_file {
   local src=$1 dst=$2
 
+  if [ -z "$(diff -q $src $dst)" ]; then
+    return
+  fi
+
   if [ -e $dst ]; then
     echo "File $dst already exists!"
     echo -n "Do you want to overwrite? [y/n] "
     read action
-    case $action in
-      y|Y)
-        ;;
-      *)
-        return
-    esac
+    if [ $action = "y" ] || [ $action = "Y" ]; then
+      ln -fs $src $dst
+    fi
   fi
-
-  ln -sf $src $dst
 }
 
 cd $(dirname $0)
