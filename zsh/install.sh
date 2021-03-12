028027516c224aea775f2f2ad2dc9601a04a9408
Injae Kang <abcinje@gmail.com>
2021-03-13 05:35:37 +0900

    Make it install Oh My Zsh and zsh-syntax-highlighting

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,14 @@
-link_file $dotfiles_root/zsh/zshrc $HOME/.zshrc
+omz=$HOME/.oh-my-zsh
 
-if [ -d $HOME/.oh-my-zsh ]; then
-  link_file $dotfiles_root/zsh/injae.zsh-theme $HOME/.oh-my-zsh/custom/themes/injae.zsh-theme
+if [ ! -d $omz ]; then
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+  rm $HOME/.zshrc
+fi
+
+link_file $dotfiles_root/zsh/zshrc $HOME/.zshrc
+link_file $dotfiles_root/zsh/injae.zsh-theme $omz/custom/themes/injae.zsh-theme
+
+if [ ! -d $omz/custom/plugins/zsh-syntax-highlighting ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+      $omz/custom/plugins/zsh-syntax-highlighting
 fi
