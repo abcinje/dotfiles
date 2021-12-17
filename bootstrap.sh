ded703833b9e0b476559834058fde63edbd665f0
Injae Kang <abcinje@gmail.com>
2021-12-18 06:10:51 +0900

    Make the bootstrap script take parameters

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,32 @@ dotfiles_root=$(pwd)
 
 source util.sh
 
-for file in $(ls); do
-  if [ -f $file/install.sh ]; then
-    echo "Configuring $file..."
-    source $file/install.sh
+if [ $# -gt 0 ]; then
+  error=false
+  for dir in $@; do
+    if [ ! -d $dir ]; then
+      error=true
+      echo "Invalid argument: $dir"
+    fi
+  done
+
+  if $error; then
+    echo "Aborting."
+    exit 1
   fi
+
+  list=$@
+else
+  for file in $(ls); do
+    if [ -d $file ]; then
+      list="$list $file"
+    fi
+  done
+fi
+
+for dir in $list; do
+  echo "Configuring $dir..."
+  source $dir/install.sh
 done
+
 echo "Done."
