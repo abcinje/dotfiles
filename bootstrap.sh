cc83e8c8b77721275ebd58c688ad44cc5fd511c0
Injae Kang <abcinje@gmail.com>
2021-12-18 04:52:24 +0900

    Fix minor

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,10 @@ dotfiles_root=$(pwd)
 
 source util.sh
 
-for dir in $(ls); do
-  if [ -f $dir/install.sh ]; then
-    echo "Configuring $dir..."
-    source $dir/install.sh
+for file in $(ls); do
+  if [ -f $file/install.sh ]; then
+    echo "Configuring $file..."
+    source $file/install.sh
   fi
 done
 echo "Done."
