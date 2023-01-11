f4634c2867d0d473800d69f7f87054f80542ca56
Injae Kang <abcinje@gmail.com>
2023-01-12 01:43:25 +0900

    Update the LunarVim setting

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,6 +1,4 @@
+vim.opt.linebreak = true
+vim.opt.relativenumber = true
 vim.opt.title = false
-
-lvim.leader = ","
-
-lvim.keys.insert_mode["jk"] = "<Esc>"
-lvim.keys.normal_mode["<Leader><Space>"] = ":nohlsearch<CR>"
+vim.opt.wrapscan = false
