d7f41d8745d1e91b77deffbee2861ec82690dc0a
Injae Kang <abcinje@gmail.com>
2023-01-17 15:13:18 +0900

    Enable Hop

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,3 +3,34 @@ vim.opt.relativenumber = true
 vim.opt.title = false
 vim.opt.wrap = true
 vim.opt.wrapscan = false
+
+lvim.plugins = {
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+
+      hop.setup()
+      vim.keymap.set("n", "f", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end)
+      vim.keymap.set("n", "F", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end)
+      vim.keymap.set("n", "t", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end)
+      vim.keymap.set("n", "T", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = -1 })
+      end)
+      vim.keymap.set("n", "s", function()
+        hop.hint_words({ direction = directions.AFTER_CURSOR })
+      end)
+      vim.keymap.set("n", "S", function()
+        hop.hint_words({ direction = directions.BEFORE_CURSOR })
+      end)
+    end,
+  },
+}
