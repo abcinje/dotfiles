35c554b789d0f91d32e02bdc69aa4c565af0f428
Injae Kang <abcinje@gmail.com>
2023-04-16 12:13:55 +0900

    Enable neoscroll.nvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -35,4 +35,11 @@ lvim.plugins = {
     "ggandor/lightspeed.nvim",
     event = { "BufNewFile", "BufRead" },
   },
+
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
 }
