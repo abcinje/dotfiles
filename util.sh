9ff13db98beada6e9629b0ae3b66a1aeb566b6f9
Injae Kang <abcinje@gmail.com>
2023-07-28 11:16:32 +0900

    Update util.sh

--- util.sh
+++ util.sh
@@ -3,16 +3,30 @@ function eecho {
 }
 
 function link_file {
-  local src=$1 dst=$2
+  local src="$1" dst="$2"
 
-  if [ ! -e $dst ]; then
-    ln -fs $src $dst
-  elif [ -n "$(diff -q $src $dst)" ]; then
-    echo "File \"$dst\" already exists!"
-    echo -n "Do you want to overwrite? [y/n] "
-    read action
-    if [ $action = "y" ] || [ $action = "Y" ]; then
-      ln -fs $src $dst
-    fi
+  if [ ! -e "$src" ]; then
+    eecho "  File \"$src\" does not exists."
+    eecho "  Aborting."
+    return
   fi
+
+  if [ -e "$dst" ]; then
+    echo "  File \"$dst\" already exists!"
+    echo -n "  Do you want to overwrite? [y/n] "
+
+    while true; do
+      read action
+      if [ "$action" = "y" ] || [ "$action" = "Y" ]; then
+        rm -rf "$dst"
+        break
+      elif [ "$action" = "n" ] || [ "$action" = "N" ]; then
+        return
+      else
+        echo -n "  Please enter \"y\" or \"n\". [y/n] "
+      fi
+    done
+  fi
+
+  ln -sf "$src" "$dst"
 }
