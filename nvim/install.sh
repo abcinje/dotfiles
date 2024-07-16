54a71ab69ba9a77a0a207df3c92ca0887a36f802
Injae Kang <abcinje@gmail.com>
2024-07-16 15:32:15 +0900

    Update nvim/install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -7,3 +7,6 @@ wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.
 tar -xf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
 EOF
+
+mkdir -p $HOME/.config/nvim
+link_file $dotfiles_root/nvim/init.lua $HOME/.config/nvim/init.lua
