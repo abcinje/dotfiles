23714f6feeb76b40795526d020333d7cb52a5746
Injae Kang <abcinje@gmail.com>
2024-07-16 15:08:02 +0900

    Update nvim/install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
-fi
-
+sudo -s -- <<EOF
 cd /opt
 rm -r nvim-linux64
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar -xf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
+EOF
