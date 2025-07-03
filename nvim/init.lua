78ea6e4d36031e73d39843f3c9089e75f0dd0ed5
Injae Kang <abcinje@gmail.com>
2025-07-03 18:25:11 +0900

    Check for plugin updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,6 +48,7 @@ else
   vim.keymap.set("n", "<C-L>", "<C-W>l")
   vim.keymap.set("n", "<Leader>h", "<Cmd>nohlsearch<CR>")
 
+  -- Bootstrap lazy.nvim
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -61,173 +62,179 @@ else
   end
   vim.opt.rtp:prepend(lazypath)
 
+  -- Setup lazy.nvim
   require("lazy").setup({
-    {
-      "numToStr/Comment.nvim",
-      config = true,
-    },
+    spec = {
+      {
+        "numToStr/Comment.nvim",
+        config = true,
+      },
 
-    {
-      "uloco/bluloco.nvim",
-      dependencies = "rktjmp/lush.nvim",
-      config = function()
-        vim.o.termguicolors = true
-        vim.cmd.colorscheme("bluloco")
-      end,
-      priority = 1000,
-    },
+      {
+        "uloco/bluloco.nvim",
+        dependencies = "rktjmp/lush.nvim",
+        config = function()
+          vim.o.termguicolors = true
+          vim.cmd.colorscheme("bluloco")
+        end,
+        priority = 1000,
+      },
 
-    {
-      "akinsho/bufferline.nvim",
-      dependencies = "nvim-tree/nvim-web-devicons",
-      config = function()
-        vim.o.termguicolors = true
-        require("bufferline").setup({ options = { show_buffer_icons = false } })
-      end,
-    },
+      {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+          vim.o.termguicolors = true
+          require("bufferline").setup({ options = { show_buffer_icons = false } })
+        end,
+      },
 
-    {
-      "github/copilot.vim",
-    },
+      {
+        "github/copilot.vim",
+      },
 
-    {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        vim.o.signcolumn = "yes"
-        local gitsigns = require("gitsigns")
-        gitsigns.setup({
-          on_attach = function(bufnr)
-            local function map(mode, l, r, opts)
-              opts = opts or {}
-              opts.buffer = bufnr
-              vim.keymap.set(mode, l, r, opts)
+      {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+          vim.o.signcolumn = "yes"
+          local gitsigns = require("gitsigns")
+          gitsigns.setup({
+            on_attach = function(bufnr)
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
+              map("n", "<Leader>gs", gitsigns.stage_hunk)
+              map("n", "<Leader>gr", gitsigns.reset_hunk)
+              map("v", "<Leader>gs", function()
+                gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+              end)
+              map("v", "<Leader>gr", function()
+                gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+              end)
             end
-            map("n", "<Leader>gs", gitsigns.stage_hunk)
-            map("n", "<Leader>gr", gitsigns.reset_hunk)
-            map("v", "<Leader>gs", function()
-              gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-            end)
-            map("v", "<Leader>gr", function()
-              gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-            end)
-          end
-        })
-      end,
-    },
+          })
+        end,
+      },
 
-    {
-      "ggandor/leap.nvim",
-      config = function()
-        require("leap").add_default_mappings()
-      end,
-    },
+      {
+        "ggandor/leap.nvim",
+        config = function()
+          require("leap").add_default_mappings()
+        end,
+      },
 
-    {
-      "nvim-lualine/lualine.nvim",
-      opts = {
-        options = {
-          component_separators = "│",
-          section_separators = "",
-          globalstatus = true,
-        },
-        sections = {
-          lualine_c = {
-            {
-              "filename",
-              path = 3,
+      {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+          options = {
+            component_separators = "│",
+            section_separators = "",
+            globalstatus = true,
+          },
+          sections = {
+            lualine_c = {
+              {
+                "filename",
+                path = 3,
+              },
             },
           },
         },
       },
-    },
 
-    {
-      "williamboman/mason.nvim",
-      config = true,
-    },
+      {
+        "williamboman/mason.nvim",
+        config = true,
+      },
 
-    {
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-      config = function()
-        require("mason-lspconfig").setup({ automatic_installation = true })
-        require("lspconfig").lua_ls.setup({
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = {
-                  "vim",
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+        config = function()
+          require("mason-lspconfig").setup({ automatic_installation = true })
+          require("lspconfig").lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = {
+                    "vim",
+                  },
                 },
               },
             },
-          },
-        })
-        require("lspconfig").rust_analyzer.setup({})
-      end,
+          })
+          require("lspconfig").rust_analyzer.setup({})
+        end,
+      },
+
+      {
+        "windwp/nvim-autopairs",
+        config = true,
+      },
+
+      {
+        "ethanholz/nvim-lastplace",
+        config = true,
+      },
+
+      {
+        "dstein64/nvim-scrollview",
+      },
+
+      {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+          vim.g.loaded_netrw = 1
+          vim.g.loaded_netrwPlugin = 1
+          vim.o.termguicolors = true
+          require("nvim-tree").setup()
+          vim.keymap.set("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>")
+        end,
+      },
+
+      {
+        "nvim-telescope/telescope.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = function()
+          local builtin = require("telescope.builtin")
+          vim.keymap.set("n", "<Leader>f", function()
+            local opts = { previewer = false }
+            if not pcall(builtin.git_files, opts) then
+              builtin.find_files(opts)
+            end
+          end)
+          vim.keymap.set("n", "<Leader>t", function()
+            local git_root = vim.fn.system("git rev-parse --show-toplevel")
+            if vim.v.shell_error == 0 then
+              builtin.live_grep({ cwd = vim.trim(git_root) })
+            else
+              builtin.live_grep()
+            end
+          end)
+          vim.keymap.set("n", "<Leader>r", function()
+            builtin.oldfiles({ previewer = false })
+          end)
+        end,
+      },
+
+      --[[
+      {
+        "mcchrish/zenbones.nvim",
+        dependencies = "rktjmp/lush.nvim",
+        config = function()
+          vim.o.background = "light"
+          vim.o.termguicolors = true
+          vim.cmd.colorscheme("zenwritten")
+        end,
+        priority = 1000,
+      },
+      --]]
     },
 
-    {
-      "windwp/nvim-autopairs",
-      config = true,
-    },
-
-    {
-      "ethanholz/nvim-lastplace",
-      config = true,
-    },
-
-    {
-      "dstein64/nvim-scrollview",
-    },
-
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = "nvim-tree/nvim-web-devicons",
-      config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        vim.o.termguicolors = true
-        require("nvim-tree").setup()
-        vim.keymap.set("n", "<Leader>e", "<Cmd>NvimTreeToggle<CR>")
-      end,
-    },
-
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = "nvim-lua/plenary.nvim",
-      config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<Leader>f", function()
-          local opts = { previewer = false }
-          if not pcall(builtin.git_files, opts) then
-            builtin.find_files(opts)
-          end
-        end)
-        vim.keymap.set("n", "<Leader>t", function()
-          local git_root = vim.fn.system("git rev-parse --show-toplevel")
-          if vim.v.shell_error == 0 then
-            builtin.live_grep({ cwd = vim.trim(git_root) })
-          else
-            builtin.live_grep()
-          end
-        end)
-        vim.keymap.set("n", "<Leader>r", function()
-          builtin.oldfiles({ previewer = false })
-        end)
-      end,
-    },
-
-    --[[
-    {
-      "mcchrish/zenbones.nvim",
-      dependencies = "rktjmp/lush.nvim",
-      config = function()
-        vim.o.background = "light"
-        vim.o.termguicolors = true
-        vim.cmd.colorscheme("zenwritten")
-      end,
-      priority = 1000,
-    },
-    --]]
+    -- Check for plugin updates
+    checker = { enabled = true },
   })
 end
